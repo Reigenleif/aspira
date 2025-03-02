8bfe122181e8c5637f77129f284f8abf38a6b082
set <set>
2025-03-02 17:33:40 +0000

    refactor: adding ollama folder

--- be/app/app.py
+++ be/app/app.py
@@ -30,4 +30,4 @@ app.include_router(hooker, prefix=BASE_URL)
 if __name__ == "__main__":
     import asyncio
     import fastapi
-    fastapi.run(app, host="0.0.0.0", port=8000)
+    fastapi.run(app, host="0.0.0.0", port=9000)
